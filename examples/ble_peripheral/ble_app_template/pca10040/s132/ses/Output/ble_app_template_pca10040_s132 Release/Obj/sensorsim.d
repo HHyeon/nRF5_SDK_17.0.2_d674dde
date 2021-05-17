aa5fa9938c73d4ed5098af48b810f4470bdad8d5
HHyeon <sunki1123@naver.com>
2021-05-17 15:49:39 +0900

    first commit and ble_app_templete is blinky function ready

---
+++ examples/ble_peripheral/ble_app_template/pca10040/s132/ses/Output/ble_app_template_pca10040_s132 Release/Obj/sensorsim.d
@@ -0,0 +1,5 @@
+Output/ble_app_template_pca10040_s132\ Release/Obj/sensorsim.o: \
+ C:\Users\HYEON-desk\Desktop\NRF52832DK\nRF5_SDK_17.0.2_d674dde\components\libraries\sensorsim\sensorsim.c \
+ C:\Users\HYEON-desk\Desktop\NRF52832DK\nRF5_SDK_17.0.2_d674dde\components\libraries\sensorsim\sensorsim.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.50/include/stdint.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.50/include/stdbool.h
